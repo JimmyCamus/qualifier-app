7b5766c2620c4e97e7b0f7c0c5f318e04ba5814a
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-04 15:35:45 -0300

    feat: layout has been created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Montserrat } from "@next/font/google";
+import Layout from "../components/layout";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={montserrat.className}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   );
 }
