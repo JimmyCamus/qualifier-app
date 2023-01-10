fdd554792325d25c51e0a543a6dc669436974c41
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-10 11:01:05 -0300

    feat: login endpoint has been created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,43 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { Montserrat } from "@next/font/google";
 import Layout from "../components/layout";
+import { UserProvider } from "../lib/contexts/user.context";
+import { NextPageContext } from "next";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, user }: any) {
   return (
     <main className={montserrat.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} user={user} />
+        </Layout>
+      </UserProvider>
     </main>
   );
 }
+
+App.getInitialProps = async ({ ctx }: { ctx: NextPageContext }) => {
+  let user = {};
+  if (ctx.req?.headers.cookie) {
+    const token = ctx.req?.headers.cookie?.split("=")[1];
+    const userDataResponse = await fetch(
+      `${process.env.SERVER_URL}/auth/profile`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "aplication/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    user = await userDataResponse.json();
+  }
+
+  return {
+    user,
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import GameList from "../components/GameList";
+import { useSetUserData } from "../hooks/user.hooks";
+import { useUser } from "../lib/contexts/user.context";
 import { Game } from "../lib/types/game.type";
+import { User } from "../lib/types/user.type";
 
-const Home = ({ games }: { games: Game[] }) => {
+const Home = ({ games, user }: { games: Game[]; user: User }) => {
+  const userContext = useUser();
+  useSetUserData(user, userContext);
   return (
     <>
       <GameList games={games} />
