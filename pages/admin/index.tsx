cdebac144365cffc8efac035e61cd2c38fc6503c
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-13 19:15:46 -0300

    feat: admin page has been created

---
+++ pages/admin/index.tsx
@@ -0,0 +1,52 @@
+import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import AdminRoute from "../../components/admin/AdminRoute";
+import CreateGameModal from "../../components/admin/CreateGameModal";
+import GamesTable from "../../components/admin/GamesTable";
+import { useCreateGame } from "../../hooks/admin.hooks";
+import { CreateGameFormType } from "../../lib/types/entries.type";
+import { Game } from "../../lib/types/game.type";
+import { User } from "../../lib/types/user.type";
+import { getUser } from "../../utils/user.utils";
+
+const AdminPage = ({ games, user }: { games: Game[]; user: User }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const handleCreateGame = useCreateGame();
+  const handleCreateGames = async (formInputs: CreateGameFormType) => {
+    setIsLoading(true);
+    await handleCreateGame(formInputs);
+    router.reload();
+    setIsLoading(false);
+  };
+  return (
+    <AdminRoute user={user}>
+      {isLoading ? <progress className="progress w-full"></progress> : null}
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-col w-[90vw]">
+          <div className="mb-4">
+            <CreateGameModal handleCreateGame={handleCreateGames} />
+          </div>
+          <GamesTable games={games} />
+        </div>
+      </div>
+    </AdminRoute>
+  );
+};
+
+export default AdminPage;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const user = await getUser(context.req);
+  const data = await fetch(`${process.env.CLIENT_URL}/api/games/all`);
+  const { games } = await data.json();
+  return {
+    props: {
+      games,
+      user,
+    },
+  };
+};
